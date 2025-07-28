ab5193d8d1b3a597763bb71d8913e7108307fef9
sub-bong <kim5ys@naver.com>
2025-07-29 07:52:41 +0900

    update: 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills(),
-  ],
-})
+  plugins: [react(), nodePolyfills()],
+  base: "/",
+});
